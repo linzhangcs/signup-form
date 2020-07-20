8f111b795f1d7425eeb8d7ac330e04bfeb0c00af
Lin Zhang <lin.zhang.ux@gmail.com>
2020-07-20 16:18:24 -0700

    change background color

--- signupform/src/styles/global.js
+++ signupform/src/styles/global.js
@@ -1,11 +1,11 @@
 export const colors = {
     white: "#fffdff",
-    primaryRed: "rgba(255, 20, 30, 0.6)",
+    primaryRed: "rgba(255, 121, 120, 0.99)",
     primaryGreen: "hsl(154, 59%, 51%)",
     accentBlue: "hsl(248, 32%, 49%)",
     neutralDarkBlue: "hsl(249, 10%, 26%)",
     neutralGrayishBlue: "hsl(246, 25%, 77%)",
-    // neutralGrayishBlue: "rgba(185, 182, 211, 0.6)"
+    grayishBlue: "rgba(185, 182, 211, 0.6)",
     purpleBlue: "rgba(62, 60, 73, 0.3)"
 };
 
