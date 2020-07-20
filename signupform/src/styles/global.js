a8395755ed79c90f6c29b4ba0b7da73d98dac7af
Lin Zhang <lin.zhang.ux@gmail.com>
2020-07-19 23:38:18 -0700

    add initial react app setup, github page deployment and styled component

---
+++ signupform/src/styles/global.js
@@ -0,0 +1,13 @@
+export const colors = {
+    primaryRed: "hsl(0, 100%, 74%)",
+    primaryGreen: "hsl(154, 59%, 51%)",
+    accentBlue: "hsl(248, 32%, 49%)",
+    neutralDarkBlue: "hsl(249, 10%, 26%)",
+    neutralGrayishBlue: "hsl(246, 25%, 77%)"
+};
+
+export const setting  = {
+    mobile: "375px",
+    desktop: "1440px",
+    fontSize: "16px"
+};
