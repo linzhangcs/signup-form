3d1c6ab03b101978ec14c2dd4517f94c6f2d3b5a
Lin Zhang <lin.zhang.ux@gmail.com>
2020-07-20 14:51:44 -0700

    convert some colors from hsl to rgba

--- signupform/src/styles/global.js
+++ signupform/src/styles/global.js
@@ -1,10 +1,12 @@
 export const colors = {
     white: "#fffdff",
-    primaryRed: "rgba(255, 30, 30, 0.6)",
+    primaryRed: "rgba(255, 20, 30, 0.6)",
     primaryGreen: "hsl(154, 59%, 51%)",
     accentBlue: "hsl(248, 32%, 49%)",
     neutralDarkBlue: "hsl(249, 10%, 26%)",
-    neutralGrayishBlue: "hsl(246, 25%, 77%)"
+    neutralGrayishBlue: "hsl(246, 25%, 77%)",
+    // neutralGrayishBlue: "rgba(185, 182, 211, 0.6)"
+    purpleBlue: "rgba(62, 60, 73, 0.3)"
 };
 
 export const setting  = {
